05ec5d7e426aeb7d0613cd4d8e8e5658084460b8
Marcos Imfeld <sebastianimfeld@gmail.com>
2022-08-26 17:10:35 -0300

    working in tests

--- test/expenses.js
+++ test/expenses.js
@@ -60,7 +60,7 @@ describe("Expenses", () => {
             updatedBy: res.body.id,
           });
 
-          oneBudget.save().then((res) => {
+          oneBudget.save((err, budget) => {
             chai
               .request(server)
               .get("/api/v1/budgets")
@@ -69,10 +69,16 @@ describe("Expenses", () => {
                 let anExpense = new Expense({
                   name: "Pollo",
                   amount: 4500,
-                  budget: res.body.data._id,
+                  budget: budget.id,
                 });
 
-                anExpense.save().then((res) => {
+                let anotherExpense = new Expense({
+                  name: "Pepsi",
+                  amount: 230,
+                  budget: budget.id,
+                });
+
+                Expense.create(anExpense, anotherExpense).then((res) => {
                   chai
                     .request(server)
                     .get("/api/v1/expenses")
@@ -80,10 +86,53 @@ describe("Expenses", () => {
                     .end((err, res) => {
                       res.body.status.should.be.eql(200);
                       res.body.data.should.be.a("array");
-                      res.body.total.should.be.eql(1);
+                      res.body.total.should.be.eql(2);
                       done();
                     });
                 });
+              });
+          });
+        });
+    });
+  });
+
+  describe("/POST expenses", () => {
+    it("it should POST an expense", (done) => {
+      chai
+        .request(server)
+        .post("/api/v1/users/login")
+        .send({ email: EMAIL, password: PASSWORD })
+        .end((err, res) => {
+          let token = res.header["auth-token"];
+
+          let oneBudget = new Budget({
+            name: "Comida",
+            expectedAmount: 10000,
+            createdBy: res.body.id,
+            updatedBy: res.body.id,
+          });
+
+          oneBudget.save((err, budget) => {
+            let anExpense = new Expense({
+              name: "Pollo",
+              amount: 4500,
+              budget: budget.id,
+            });
+            chai
+              .request(server)
+              .post("/api/v1/expenses")
+              .set("auth-token", token)
+              .send(anExpense)
+              .end((err, res) => {
+                res.body.status.should.be.eql(201);
+                res.body.isStored.should.be.eql(true);
+                res.body.data.should.be.a("object");
+                res.body.data.should.have.property("name");
+                res.body.data.should.have.property("amount");
+                res.body.data.should.have.property("budget");
+                res.body.data.should.have.property("name").eql("Pollo");
+                res.body.data.should.have.property("amount").eql(4500);
+                res.body.data.should.have.property("budget").eql(oneBudget.id);
                 done();
               });
           });

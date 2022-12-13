97b23995283e9843ccbdf2f9a7af6d33f97444b6
Marcos Imfeld <sebastianimfeld@gmail.com>
2022-12-13 15:43:18 -0300

    .

--- src/middlewares/validate-token.js
+++ src/middlewares/validate-token.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 const verifyToken = (req, res, next) => {
-  const token = req.header("auth-token");
+  const token = req.header("Authorization");
 
   if (!token) {
     return res.status(401).json({

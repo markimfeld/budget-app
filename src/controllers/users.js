d0c79ae7f411ea473b57b2ed626a159cf86f1835
Marcos Imfeld <sebastianimfeld@gmail.com>
2023-09-08 00:20:52 -0300

    .

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -84,7 +84,7 @@ const usersController = {
     //   })
     //   .then((res) => console.log(res));
 
-    res.cookie("jwt", token);
+    res.cookie("jwt", token, { httpOnly: true, secure: false });
 
     return res.status(200).json({
       status: 200,

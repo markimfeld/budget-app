6f344e210ad838c37e1f87c16c428e209f5cb86a
Marcos Imfeld <sebastianimfeld@gmail.com>
2023-09-12 01:33:36 -0300

    .

--- src/app.js
+++ src/app.js
@@ -31,9 +31,7 @@ mongoose
 const app = express();
 
 app.use(cookieParser());
-app.use(
-  cors({ origin: ["https://budget-app-rouge.vercel.app"], credentials: true })
-);
+app.use(cors({ origin: [`${process.env.BASE_URL_FRONT}`], credentials: true }));
 // app.use(
 //   cors({
 //     origin: (origin, callback) => {
